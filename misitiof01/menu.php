8a51433ec61e23cdd6507ad3ac8a7042e4cc6682
alejandraramirez-alt <alejandranazareht@gmail.com>
2026-07-07 22:51:53 -0600

    Add files via upload

---
+++ misitiof01/menu.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./imgfares/favicon.png" type="image/png" sizes="16x16">
+    <link rel="stylesheet" type="text/css" href="./EstilosCssF/dcoloresf.css">
+    <link rel="stylesheet" type="text/css" href="./EstilosCssF/w3.css">
+    <script src="./JScript/acciones_script.js"></script>
+    <title>Fares</title>
+</head>
+<body>
+
+    <!-- Encabezado -->
+    <div class="w3-container">
+        <header class="w3-container fcolor-d5">
+            <h1>Ediciones Fares</h1>
+        </header>
+    </div>
+
+    <!-- Navegación -->
+    <nav>
+        <a href="#">Principal</a>
+        <button>Libros</button>
+        <div>
+            <a href="#">Educación Básica</a>
+            <a href="#">I BTP y I BCH</a>
+            <a href="#">II BTP</a>
+            <a href="#">III BTP</a>
+        </div>
+        <div>
+            <button>Inventario</button>
+            <a href="cproductos.php">Crear productos</a>
+            <a href="#">Consultar y modificar producto</a>
+            <a href="#">Clientes</a>
+            <a href="#">Agregar inventario</a>
+            <a href="#">Facturar</a>
+            <a href="#">Información libros</a>
+        </div>
+        <a href="#">Contacto</a>
+    </nav>
+
+</body>
+</html>
